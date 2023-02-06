3ed6f7ccaf16e9f62337be0dcc227684dfd07bd1
Anastasiia <anasta_ok@mail.ru>
2023-02-06 18:28:44 +0300

    creating a fetchPosts async thunk

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,34 +1,22 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
-import { sub } from "date-fns";
+import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = [
-  {
-    id: "1",
-    title: "Learning Redux Toolkit",
-    content: "I've heard good things",
-    date: sub(new Date(), { minutes: 10 }).toISOString(),
-    reactions: {
-      thumbsUp: 3,
-      wow: 1,
-      heart: 5,
-      rocket: 10,
-      coffee: 2,
-    },
-  },
-  {
-    id: "2",
-    title: "Slices...",
-    content: "The more I say slice, the more I want pizza",
-    date: sub(new Date(), { minutes: 5 }).toISOString(),
-    reactions: {
-      thumbsUp: 10,
-      wow: 0,
-      heart: 2,
-      rocket: 0,
-      coffee: 3,
-    },
-  },
-];
+const POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
+
+const initialState = {
+  posts: [],
+  status: "idle", // "idle", "loading", "succeseded"
+  error: null,
+};
+
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+  try {
+    const response = await axios.get(POSTS_URL);
+    return [...response.data];
+  } catch (err) {
+    return err.message;
+  }
+});
 
 const postsSlice = createSlice({
   name: "posts",
