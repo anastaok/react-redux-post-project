af711b97c612f4e37a4d79ab22774ff02ed48f39
Anastasiia <anasta_ok@mail.ru>
2023-02-07 17:24:10 +0300

    added PostsExcerpt component and displayng the loding state

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -6,43 +6,39 @@ import {
   getPostsStatus,
   fetchPosts,
 } from "./postsSlice";
-import PostAuthor from "./PostAuthor";
-import TimeAgo from "./TimeAgo";
-import ReactionButtons from "./ReactionButtons";
+import PostsExcerpt from "./PostsExcerpt";
 
 const PostsList = () => {
   const dispatch = useDispatch();
 
   const posts = useSelector(selectAllPosts);
-  const postsStatus = useSelector(getPostsStatus);
+  const postStatus = useSelector(getPostsStatus);
   const error = useSelector(getPostsError);
 
   useEffect(() => {
-    if (postsStatus === "idle") {
+    if (postStatus === "idle") {
       dispatch(fetchPosts());
     }
-  }, [postsStatus, dispatch]);
+  }, [postStatus, dispatch]);
 
-  const orderedPost = posts
-    .slice()
-    .sort((a, b) => b.date.localeCompare(a.date));
-
-  const renderedPosts = orderedPost.map((post) => (
-    <article key={post.id}>
-      <h3>{post.title}</h3>
-      <p>{post.content.substring(0, 100)}</p>
-      <p className="postCredit">
-        <PostAuthor userId={post.userId} />
-        <TimeAgo timestamp={post.date} />
-      </p>
-      <ReactionButtons post={post} />
-    </article>
-  ));
+  let content;
+  if (postStatus === "loading") {
+    content = <p>"Loading..."</p>;
+  } else if (postStatus === "succeeded") {
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date));
+    content = orderedPosts.map((post) => (
+      <PostsExcerpt key={post.id} post={post} />
+    ));
+  } else if (postStatus === "failed") {
+    content = <p>{error}</p>;
+  }
 
   return (
     <section>
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   );
 };
