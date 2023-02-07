bc72cd0d1baf42a922c94434ebe11caac6571a51
Anastasiia <anasta_ok@mail.ru>
2023-02-07 19:17:20 +0300

    creating and dispatcing fetchUsers at App load time

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
+import { fetchUsers } from "./features/users/usersSlise";
 
 import "./index.css";
 import App from "./App";
+
+store.dispatch(fetchUsers());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
